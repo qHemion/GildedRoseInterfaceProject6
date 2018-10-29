1b59e9b41338e7291227a42f7be67caebd13b152
qHemion <q>
2018-10-29 15:39:47 +0100

    Test

--- src/main/java/edu/insightr/gildedrose/Visitor.java
+++ src/main/java/edu/insightr/gildedrose/Visitor.java
@@ -2,7 +2,21 @@ package edu.insightr.gildedrose;
 
 public class Visitor {
 
+    private final String Vest;
+    private final String Brie;
+    private final String Sulfuras;
+    private final String ETC;
+    private final String Elixir;
+    private final String ManaBun;
 
+    {
+        Elixir = "Elixir of the Mongoose";
+        ETC = "Backstage passes to a TAFKAL80ETC concert";
+        Sulfuras = "Sulfuras, Hand of Ragnaros";
+        Brie = "Aged Brie";
+        Vest = "+5 Dexterity Vest";
+        ManaBun = "Conjured Mana Cake";
+    }
 
     void updateQuality(Item myItem){
         myItem.setQuality(0);
